6885a58539a7c2935349521ebbaa72752f51b77c
Aditya Pillai <github@pillai.io>
2018-11-18 13:53:45 -0500

    Added particles

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,11 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+	console.log('callback - particles.js config loaded');
+  });
+
 (function() {
 
 	"use strict";
@@ -42,7 +47,7 @@
 						},
 
 					// Delay.
-						delay: 6000
+						delay: 5000
 
 				};
 
