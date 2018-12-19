86c7db7420d75b77460bb5f652edcb87f765230f
Aditya Pillai <github@pillai.io>
2018-12-19 17:23:16 -0600

    Validate JSON with ajv

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
-
 const router = express.Router();
 
-/* GET home page. */
+// JSON Validation
+const Ajv = require('ajv');
+const ajv = new Ajv();
+const schema = require('./../schema/courses.json')
+const validate = ajv.compile(schema);
+
+/* GET Request */
+
+// Validate JSON data
 router.get('/', (req, res) => {
-  res.send('This does nothing');
+  
+  if (!req.body) return res.send(400);
+
+  console.log(req.body);
+  
+  var valid = validate(req.body);
+  if (!valid) {
+    console.log("\nERRORS:\n")
+    console.log(validate.errors);
+    return res.send(400);
+  }
+
+  return res.send("Look to console for details...");
+
 });
 
 module.exports = router;
