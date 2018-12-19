184eb8fc914c7fa38624de02642e42701a833824
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2018-12-19 15:16:13 -0500

    Extract course planning info

--- app/scripts.babel/course-planner.js
+++ app/scripts.babel/course-planner.js
@@ -1,3 +1,3 @@
 'use strict';
 
-console.log(test);
+console.log(data);
--- app/scripts.babel/extractor.js
+++ app/scripts.babel/extractor.js
@@ -1,4 +1,10 @@
 'use strict';
 
 const data = {};
-const courses = [...document.querySelectorAll('.gwt-course-list .v-pnl .h-pnl .txt')].map(a => a.innerHTML.split(' :: ')[0]);
+const courseBoxes = [...document.querySelectorAll('.header.gwt-course-headerpadding')];
+data.courses = courseBoxes.map((course) => {
+  const number = course.querySelector('.title .h-pnl .txt').innerHTML.slice(0, 5);
+  const info = [...course.querySelectorAll('.h-pnl .v-pnl .valign-t .txt')];
+  const section = info.map(item => item.innerHTML[0]);
+  return { number, section };
+});
